6d74570257581b0ef3ef9bcb7648717cd28dae1a
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-10-31 15:51:37 -0300

    main form styles completed

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -15,11 +15,30 @@ function JournalForm({ onSubmit }) {
       action="submit"
       onSubmit={addJournalItem}
     >
-      <input type="text" name="title" />
-      <input type="date" name="date" />
-      <input type="text" name="tag" />
-      <textarea name="text" id="" cols="30" rows="10"></textarea>
-      <Button onClick={() => console.log("нажали")} text="Сохранить" />
+      <div className={styles["form-title-wrapper"]}>
+        <input type="text" name="title" className={styles["form-title"]} />
+        <img src="/deleteIcon.svg" alt="deleteIcon" />
+      </div>
+      <div className={styles["form-input-wrapper"]}>
+        <img src="/dateIcon.svg" alt="dateIcon" />
+        <label htmlFor="date">Дата</label>
+        <input type="date" name="date" id="date" />
+      </div>
+      <div className={styles["form-input-wrapper"]}>
+        <img src="/folderIcon.svg" alt="folderIcon" />
+        <label htmlFor="tag">Метки</label>
+        <input type="text" name="tag" id="tag" />
+      </div>
+      <textarea
+        className={styles["text-area"]}
+        name="text"
+        id=""
+        cols="30"
+        rows="10"
+      />
+      <div className={styles["form-button"]}>
+        <Button onClick={() => console.log("нажали")} text="Сохранить" />
+      </div>
     </form>
   );
 }
