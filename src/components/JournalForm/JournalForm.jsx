3de3f9732d6c343241547a060ab239de755a9a19
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-01 14:34:45 -0300

    Added useReducer hook for managing form state

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useReducer } from "react";
 import Button from "../Button/Button";
 import styles from "./JournalForm.module.css";
+import { INITIAL_STATE, formReducer } from "./JournalForm.state";
 
 function JournalForm({ onSubmit }) {
+  const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+  const { isFormReadyToSubmit, values } = formState;
+
+  useEffect(() => {
+    if (isFormReadyToSubmit) {
+      onSubmit(values);
+      dispatchForm({ type: "CLEAR" });
+    }
+  }, [isFormReadyToSubmit]);
+
   const addJournalItem = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-    onSubmit(formProps);
+    dispatchForm({ type: "SUBMIT" });
+  };
+
+  const onChange = (e) => {
+    dispatchForm({
+      type: "SET_VALUE",
+      payload: { [e.target.name]: e.target.value },
+    });
   };
 
   return (
@@ -16,28 +33,48 @@ function JournalForm({ onSubmit }) {
       onSubmit={addJournalItem}
     >
       <div className={styles["form-title-wrapper"]}>
-        <input type="text" name="title" className={styles["form-title"]} />
+        <input
+          type="text"
+          onChange={onChange}
+          value={values.title}
+          name="title"
+          className={styles["form-title"]}
+        />
         <img src="/deleteIcon.svg" alt="deleteIcon" />
       </div>
       <div className={styles["form-input-wrapper"]}>
         <img src="/dateIcon.svg" alt="dateIcon" />
         <label htmlFor="date">Дата</label>
-        <input type="date" name="date" id="date" />
+        <input
+          type="date"
+          onChange={onChange}
+          value={values.date}
+          name="date"
+          id="date"
+        />
       </div>
       <div className={styles["form-input-wrapper"]}>
         <img src="/folderIcon.svg" alt="folderIcon" />
         <label htmlFor="tag">Метки</label>
-        <input type="text" name="tag" id="tag" />
+        <input
+          type="text"
+          onChange={onChange}
+          value={values.tag}
+          name="tag"
+          id="tag"
+        />
       </div>
       <textarea
         className={styles["text-area"]}
         name="text"
+        onChange={onChange}
+        value={values.text}
         id=""
         cols="30"
         rows="10"
       />
       <div className={styles["form-button"]}>
-        <Button onClick={() => console.log("нажали")} text="Сохранить" />
+        <Button text="Сохранить" />
       </div>
     </form>
   );
---
+++ src/components/JournalForm/JournalForm.state.js
@@ -0,0 +1,29 @@
+export const INITIAL_STATE = {
+  values: {
+    title: "",
+    date: "",
+    tag: "",
+    text: "",
+  },
+  isFormReadyToSubmit: false,
+};
+
+export function formReducer(state, action) {
+  switch (action.type) {
+    case "SET_VALUE":
+      return { ...state, values: { ...state.values, ...action.payload } };
+
+    case "CLEAR":
+      return { ...state, values: INITIAL_STATE.values };
+
+    case "SUBMIT": {
+      const titleValidity = state.values.title?.trim().length;
+      const dateValidity = state.values.date;
+      const textValidity = state.values.text?.trim().length;
+      return {
+        ...state,
+        isFormReadyToSubmit: titleValidity && dateValidity && textValidity,
+      };
+    }
+  }
+}
