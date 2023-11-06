fd0d6facee0f80d74695ec59c23ca85a70639691
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-06 18:35:41 -0300

    added a logo Toggle in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import Button from "../Button/Button";
 import SelectUser from "../SelectUser/SelectUser";
 import styles from "./Header.module.css";
 
+const logo = ["/logo.svg", "/vite.svg"];
+
 function Header() {
+  const [logoIndex, setLogoIndex] = useState(0);
+
+  const toggleLogo = () => {
+    setLogoIndex((prev) => Number(!prev));
+  };
+
+  console.log(logoIndex);
   return (
     <>
-      <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+      <img
+        className={styles.logo}
+        src={logo[logoIndex]}
+        alt="Логотип журнала"
+      />
       <SelectUser />
+      <Button onClick={toggleLogo} text="Сменить Лого" />
     </>
   );
 }
